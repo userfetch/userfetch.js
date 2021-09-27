628bc80ec4bb3ecf31467f84d0d25671ef04fb96
aryan02420 <aryan02420@gmail.com>
2021-09-27 12:54:19 +0530

    basic terminal rendering

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-import getStats from './getStats.js'
+import githubAPI from './apis/github.js'
+import termkit from 'terminal-kit'
+import fs from 'fs'
+import dotenv from 'dotenv'
 
-let stats = await getStats()
+dotenv.config()
+
+const term = termkit.terminal
+
+let stats = await githubAPI(process.env.github_token)()
+
 console.log(stats)
+
+term.hideCursor(true)
+const cursorPos = await term.getCursorLocation()
+cursorPos.x += 5
+function info(key, value) {
+  term.moveTo(cursorPos.x, cursorPos.y)
+  term.brightGreen.bold(key)(': ')(value)
+  ++cursorPos.y
+}
+function title(value) {
+  term.moveTo(cursorPos.x, cursorPos.y)
+  term.brightGreen.bold(value)
+  ++cursorPos.y
+}
+function underline() {
+  term.moveTo(cursorPos.x, cursorPos.y)
+  term('------')
+  ++cursorPos.y
+}
+title('title')
+underline()
+info('name1', 'aryan')
+info('name2', 'aryan')
+info('name3', 'aryan')
+info('name4', 'aryan')
+info('name5', 'aryan')
+info('name6', 'aryan')
+info('name7', 'aryan')
+info('name8', 'aryan')
+console.log('')
+term.hideCursor(false)
+
+// const asciiart = fs.readFileSync('./ascii')
 
 // TODO
 // title("")
@@ -12,3 +53,12 @@ console.log(stats)
 // left()
 // right()
 // raw("", [""])
+// text("")
+// multitext("")
+// command("", "")
+
+// TODO
+// cli:
+// --token="ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+// --user="octocat"
+// --config="./path/to/config.js"
