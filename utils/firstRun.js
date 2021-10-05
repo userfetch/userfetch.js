50807fb779f0c95e298d7f4d7526a6f3c2d1437a
aryan02420 <aryan02420@gmail.com>
2021-10-05 18:23:35 +0530

    fix relative path in copyFile get token input on first run

--- utils/firstRun.js
+++ utils/firstRun.js
@@ -1,28 +1,25 @@
 import os from 'os'
 import fs from 'fs'
+import url from 'url'
 import path from 'path'
 
-const configDir = path.join(os.homedir(), '.userfetch/')
+import getAndSaveToken from './getAndSaveToken.js'
 
-const message = `To use this app you must first create a Github Personal Access Token
+const configDir = path.join(os.homedir(), '.userfetch/')
+const currDir = path.dirname(url.fileURLToPath(import.meta.url))
+
+const message = `To use this tool, you must first create a Github Personal Access Token
 Go to https://github.com/settings/tokens/new and generate a new token
 with \`repo\`, \`read:org\`, and \`user\` scopes
-Then run \`userfetch --token\` and use stdin to enter the token
-
-Run \`userfetch --help\` to get a complete list of available options
 Check https://github.com/aryan02420/userfetch/wiki/Docs for more info
 `
 
 export default async function () {
   console.log(message)
   if (!fs.existsSync(configDir)) fs.mkdirSync(configDir)
-  try {
-    await fs.promises.copyFile(
-      './stubs/.env',
-      path.join(configDir, '.env'),
-      fs.constants.COPYFILE_EXCL
-    )
-  } catch (err) {}
-  fs.promises.copyFile('./stubs/config.mjs', path.join(configDir, 'config.mjs'))
-  fs.promises.copyFile('./stubs/ascii', path.join(configDir, 'ascii'))
+  await Promise.all([
+    getAndSaveToken(),
+    fs.promises.copyFile(path.join(currDir, '..', 'stubs', 'config.mjs'), path.join(configDir, 'config.mjs')),
+    fs.promises.copyFile(path.join(currDir, '..', 'stubs', 'ascii'), path.join(configDir, 'ascii')),
+  ])
 }
