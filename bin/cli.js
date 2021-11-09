441fc77cc382bd7af8d22968544331b3c8041e6f
aryan02420 <aryan02420@gmail.com>
2021-11-09 10:30:04 +0530

    separate cli and main logic

---
+++ bin/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+import path from 'path'
+
+import ora from 'ora'
+import dotenv from 'dotenv'
+
+import { main } from '../index.js'
+import { parseArgs } from '../utils/yargs.js'
+import { firstRun } from '../utils/firstRun.js'
+import { getAndSaveToken } from '../utils/getAndSaveToken.js'
+import { CONFIG_DIR, PROJ_ROOT, CWD } from '../utils/constants.js'
+
+const args = parseArgs(process.argv)
+const spinner = ora({ spinner: 'line', color: 'gray' }).start()
+
+if (!args.ci) {
+  spinner.stop()
+  if (!fs.existsSync(CONFIG_DIR) || args.firstRun) await firstRun()
+  if (args.token && !args.firstRun) await getAndSaveToken()
+  spinner.start()
+  dotenv.config()
+  dotenv.config({ path: path.join(CONFIG_DIR, '.env') })
+}
+
+let config = {}
+if (args.config) config = await import(path.resolve(CWD, args.config))
+else if (!args.ci) config = await import(path.join(CONFIG_DIR, 'config.mjs'))
+
+let output = await main(args, process.env, config)
+spinner.stop()
+console.log(output)
+
+if (args.debug)
+  console.log({ args, githubStats, config, CONFIG_DIR, PROJ_ROOT })
--- index.js
+++ index.js
@@ -1,56 +1,25 @@
-#!/usr/bin/env node
-
-import fs from 'fs'
-import path from 'path'
-
-import ora from 'ora'
-import dotenv from 'dotenv'
-
-import { renderer as SVGRenderer } from './renderer/svg.js'
-import { parseArgs } from './utils/yargs.js'
 import * as githubAPI from './apis/github.js'
-import { firstRun } from './utils/firstRun.js'
 import { renderer } from './renderer/terminal.js'
-import { getAndSaveToken } from './utils/getAndSaveToken.js'
-import { CONFIG_DIR, PROJ_ROOT, CWD } from './utils/constants.js'
+import { renderer as SVGRenderer } from './renderer/svg.js'
 
-const args = parseArgs(process.argv)
-const spinner = ora({ spinner: 'line', color: 'gray' }).start()
-
-if (!args.ci) {
-  spinner.stop()
-  if (!fs.existsSync(CONFIG_DIR) || args.firstRun) await firstRun()
-  if (args.token && !args.firstRun) await getAndSaveToken()
-  spinner.start()
-  dotenv.config()
-  dotenv.config({ path: path.join(CONFIG_DIR, '.env') })
+export async function main(args, env, config) {
+  const template = args.user ? config.templateDefault : config.template
+  
+  githubAPI.authenticate(env.github_token)
+  const githubStats = await githubAPI.fetch(args.user)
+  
+  const output = renderer
+    .options({
+      colors: config.colors,
+      symbols: config.symbols,
+      meta: config.meta,
+    })
+    .render(template, githubStats)
+  
+  if (args.svg) {
+    let svg = SVGRenderer.options(config.svgOptions).render(output)
+    await SVGRenderer.save(svg, args.svg)
+  }
+  
+  return output
 }
-
-let config = {}
-if (args.config) config = await import(path.resolve(CWD, args.config))
-else if (!args.ci) config = await import(path.join(CONFIG_DIR, 'config.mjs'))
-const template = args.user ? config.templateDefault : config.template
-
-githubAPI.authenticate(process.env.github_token)
-const githubStats = await githubAPI.fetch(args.user)
-
-const output = renderer
-  .options({
-    colors: config.colors,
-    symbols: config.symbols,
-    meta: config.meta,
-  })
-  .render(template, githubStats)
-
-spinner.stop()
-console.log(output)
-
-spinner.start()
-if (args.svg) {
-  let svg = SVGRenderer.options(config.svgOptions).render(output)
-  await SVGRenderer.save(svg, args.svg)
-}
-spinner.stop()
-
-if (args.debug)
-  console.log({ args, githubStats, config, CONFIG_DIR, PROJ_ROOT })
